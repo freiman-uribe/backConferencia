9cc27410d4879bcce9964c69c7642d464ef268e5
Freiman Uribe <106196539+freiman-uribe@users.noreply.github.com>
2024-05-18 00:51:46 -0500

    Add files via upload

---
+++ functions/peerServer.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const { ExpressPeerServer } = require('peer');
+
+// Configuración del servidor PeerJS
+const peerServer = ExpressPeerServer(app, {
+  debug: true,
+  path: '/myapp' // Ruta para las solicitudes de PeerJS
+});
+
+// Middleware para manejar las solicitudes de PeerJS
+app.use('/myapp/peerjs', peerServer);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Servidor Express funcionando correctamente');
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
+
+// Configuración del puerto
+const PORT = process.env.PORT || 3000;
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
